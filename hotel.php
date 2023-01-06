36bcf3019d61e9f5d174c0c46e11bbf281504600
Rayan <rayanraihani9@gmail.com>
2023-01-06 16:00:28 +0100

    on crée la classe hotel ou dedans l'hotel a un nom et une adresse et on crée aussi deux liste les chambre et une liste pour oles reservation deja faite

---
+++ hotel.php
@@ -0,0 +1,16 @@
+<?php
+class Hotel{
+    private $_nom;
+    private $_adresse;
+    private array $_listeReservation;
+    private array $_listeChambre;
+
+    public function __construct($nom,$adresse){
+        $this->_nom=$nom;
+        $this->_adresse=$adresse;
+        $this->_listeReservation=[];
+        $this->_listeChambre=[];
+
+}
+
+?>
