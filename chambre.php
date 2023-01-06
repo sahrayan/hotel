986def96074c47b2542dd3f97bec7bf794f97ada
Rayan <rayanraihani9@gmail.com>
2023-01-06 16:56:50 +0100

    commentaire de la classe

---
+++ chambre.php
@@ -0,0 +1,7 @@
+<?php
+// classe chambre avec la chambre nombre, la liste de reservation ,prix,wifi,le statut reserver ou non, l hotel ,la liste de reservation
+class chambre{
+    private $_chambre
+
+
+}
