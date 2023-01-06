a0dd9e37ad55b7573f684ddcb905c0b742d0c390
Rayan <rayanraihani9@gmail.com>
2023-01-06 16:05:07 +0100

    on ajout une reservation et ca chambre puis creation d une fonction qui compte le nombre de reservation et de chambre

--- hotel.php
+++ hotel.php
@@ -10,7 +10,28 @@ class Hotel{
         $this->_adresse=$adresse;
         $this->_listeReservation=[];
         $this->_listeChambre=[];
+    }
 
+    public function GetNom(){
+        return $this->_nom;
+    }
+    public function GetAdresse(){
+        return $this->_adresse;
+    }
+    public function AddReservation($reservation){
+        $result=$this;
+        foreach ($this->_listeReservation as $reservation){
+            $result.=$reservation ."<br>";
+        }
+        return $result;
+    }
+    public function AddChambre($chambre){
+        $result=$this;
+        foreach ($this->_listeChambre as $chambre){
+            $result.=$chambre."<br>";
+        }
+        return $result;
+    }
 }
 
 ?>
