4828a64911097b2d3aa9dbf1585ca110e88ad70b
rayan <rayanraihani9@gmail.com>
2023-01-09 11:00:05 +0100

    hotel

--- hotel.php
+++ hotel.php
@@ -20,23 +20,23 @@ class Hotel{
         return $this->_adresse;
     }
 
-// ajout d une chambre dans chambre
-    public function AddChambre(){
-        $this->_listeChambre[]= $chambre;
-    }
-    public function AddReservation(){
-        $this->_listeReservation[]= $reservation;
-    }
+    
+    // ajout d une chambre dans chambre
+        public function AddChambre($chambre){
+            $this->_listeChambre[]= $chambre;
+        }
+        public function AddReservation($reservation){
+            $this->_listeReservation[]= $reservation;
+        }
 
-
-    public function dispReservation($reservation){
+    public function dispReservation(){
         $result=$this;
         foreach ($this->_listeReservation as $reservation){
             $result.=$reservation ."<br>";
         }
         return $result;
     }
-    public function dispChambre($chambre){
+    public function dispChambre(){
         $result=$this;
         foreach ($this->_listeChambre as $chambre){
             $result.=$chambre."<br>";
@@ -49,6 +49,13 @@ class Hotel{
     }    
     public function countChambres(){
         return count($this->_listeChambre);
+    }
+    public function getInfo(){
+        return $this->GetNom()."<br> ".$this->GetAdresse()."<br> ";
+    }
+    public function __toString()
+    {
+        return $this->getInfo();
     }
 
 }
