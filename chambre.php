86826457ef54e378023a4b6bb0e9a927caf728cb
rayan <rayanraihani9@gmail.com>
2023-01-09 10:59:39 +0100

    classe chambre je dois ajouter la chambre dans la liste de reservation

--- chambre.php
+++ chambre.php
@@ -1,7 +1,47 @@
 <?php
 // classe chambre avec la chambre nombre, la liste de reservation ,prix,wifi,le statut reserver ou non, l hotel ,la liste de reservation
-class chambre{
-    private $_chambre
+class Chambre{
+    private $_chambre;
+    private $_nombre;
+    private $_prix;
+    private $_wifi;
+    private $_status;
+    private Hotel $_hotel;
+    private array $_ListReservation;
 
-
+    public function __construct($chambre, $nombre, $prix, $wifi, $status,Hotel $hotel){
+     $this->_chambre = $chambre;
+     $this->_nombre = $nombre;
+     $this->_prix = $prix;
+     $this->_wifi = $wifi;
+     $this->_status = $status;
+     $this->_hotel = $hotel;
+     $this->_hotel -> AddChambre($this);
+     $this->_ListReservation =[];
+    }
+    public function AddReservation($reservation){
+        $this->_ListReservation[] = $reservation;
+    }
+    private function getChambre(){
+        return $this->_chambre;
+    }
+    private function getNombre(){
+        return $this->_nombre;
+    }
+    private function getPrix(){
+        return $this->_prix;
+    }
+    private function getWifi(){
+        return $this->_wifi;
+    }
+    private function getStatus(){
+        return $this->_status;
+    }
+    public function getInfoChambre(){
+        return $this->getChambre()." ".$this ->getNombre()." ".$this->getPrix()." ".$this->getWifi()." ".$this->getStatus()."<br>";
+    }
+    public function __toString()
+    {
+        return $this->getInfoChambre();
+    }
 }
