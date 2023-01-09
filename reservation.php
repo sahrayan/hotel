28e88ed526ac91a18fa6a4b5f83a1d1e0a68bbc0
rayan <rayanraihani9@gmail.com>
2023-01-09 15:01:41 +0100

    reserv

--- reservation.php
+++ reservation.php
@@ -1,7 +1,32 @@
 <?php
 class Reservation{
-    private Client $client;
-    private Hotel $hotel;
-    private Chambre $chambre;
+    private Client $_client;
+    private Hotel $_hotel;
+    private Chambre $_chambre;
+    private $_entree;
+    private $_sortie;
+    
+    public function __construct(Client $client, Hotel $hotel, Chambre $chambre, $entree, $sortie){
+        $this->_client = $client;
+        $this->_hotel = $hotel;
+        $this->_hotel-> AddReservation($this);
+        $this->_chambre = $chambre;
+        $this->_chambre -> AddReservation($this);
+        $this->_entree = new DateTime($entree);
+        $this->_sortie = new DateTime ($sortie);
+    }
+    public function getEntree(){
+        return $this->_entree;
+    }
+    public function getSortie(){
+        return $this->_sortie;
+    }
+    public function getInfoReservations(){
+        return "Hotel : ".$this->_hotel."".$this->_chambre."Du ".$this->getEntree()->format("d/m/y")." au ".$this->getSortie()->format("d/m/y").".";
+    }
+    public function __toString()
+    {
+        return $this->getInfoReservations();
+    }
     
 }
