5e233bb1957f478506e34aad7c144e695ac68f24
rayan <rayanraihani9@gmail.com>
2023-01-12 11:49:18 +0100

    get info reservation

--- reservation.php
+++ reservation.php
@@ -8,6 +8,7 @@ class Reservation{
     
     public function __construct(Client $client, Hotel $hotel, Chambre $chambre, $entree, $sortie){
         $this->_client = $client;
+        $this->_client->InfoClient($this);
         $this->_hotel = $hotel;
         $this->_hotel-> AddReservation($this);
         $this->_chambre = $chambre;
@@ -15,6 +16,7 @@ class Reservation{
         $this->_entree = new DateTime($entree);
         $this->_sortie = new DateTime ($sortie);
     }
+    
     public function getEntree(){
         return $this->_entree;
     }
@@ -22,28 +24,13 @@ class Reservation{
         return $this->_sortie;
     }
     // reservation info client
-    public function getInfoReservations(){
-        echo "Statuts :" .$this;
-        $result= "<table border=10>
-        <thead>
-        <tr>
-        <th>Chambre</th>
-        <th>Hotel</th>
-        
-        </tr>
-        </thead><tbody>";
-        foreach($this->_chambre as $chambre){
-            $result .= "<tr>
-            <td>$chambre</td>
-            <td>$Hotel</td>
-            </tr>";
-        }
-        $result .= "</tbody></table>";
-        echo $result;
+    public function getInfoReservation(){
+        return $this->_client. "".$this->_hotel."".$this->_chambre."Du ".$this->getEntree()->format("d/m/y")." au ".$this->getSortie()->format("d/m/y").".<br>";
+
     }
     public function __toString()
     {
-        return $this->getInfoReservations();
+        return $this->getInfoReservation();
     }
     
 }
