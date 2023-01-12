433ebc986af78b0ecb96f99c8ead8730244a85c5
rayan <rayanraihani9@gmail.com>
2023-01-12 11:48:43 +0100

    affichage html du tableau des reservation et la liste des reservation et des couleur que le client a fait

--- hotel.php
+++ hotel.php
@@ -45,18 +45,77 @@ class Hotel{
     }
     //permet de compter le nomnbre de reservations et de chambre dispo
     public function countReservations(){
-        return "il y a ".count($this->_listeReservation)." reserver<br>";
+        return "il y a ".count($this->_listeReservation)." chambres reservé<br>";
     }    
     public function countChambres(){
         return "il y a ".count($this->_listeChambre)." chambres.";
     }
+    
+    public function getInfoHotel(){
+        return $this->getInfo() . " ".$this->countChambres()." <br>".$this->countReservations();
+    }
     public function getInfo(){
-        return 'Hotel:  '.$this->GetNom()."<br> Adresse:  ".$this->GetAdresse()."<br> ";
+        return '<br>Hotel:  '.$this->GetNom()."<br> Adresse:  ".$this->GetAdresse()."<br> ";
     }
     public function __toString()
     {
         return $this->getInfo();
     }
+
+
+    public function afficherReservationHotel()
+{
+    echo "<h3>Réservations de l'hotel ".$this."</h3>"; 
+    echo "<span class='rectangle'>".(count($this->_listeReservation))." Réservations</span><br>";       
+    foreach( $this->_listeReservation as $reservation)
+    {
+       echo $reservation;
+    }
+
+}
+   
+function afficherTabChambre() {
+    $result = "<table border=1>
+                <thead>
+                    <tr>
+                    <th>Chambre</th>
+                     <th>Nombre</th>
+                     <th>Prix</th>
+                     <th>Wifi</th>
+                     <th>Etat</th>
+                    </tr>
+                </thead>
+          <tbody>" ;
+foreach ($this->_listeChambre as $chambre){
+    if ($chambre->getStatus() == "Libre")
+                {
+                    $disp= "<div class='rectangle'>Disponible</div>";
+                } 
+
+                 else 
+
+                {
+                    $disp= "<div class='rectangle2'>Réservée</div>";
+                }
+    $result .= "<tr>
+                    <td>".$chambre->getChambre()."</td> 
+                    <td>".$chambre->getNombre()."</td>
+                    <td>".$chambre->getPrix()."</td>
+                    <td>".$chambre->getWifi()."</td>
+                    <td>".$disp."</td>
+                    
+                </tr>";
+                
+
+            "</th>
+          </tr>";
+
+}
+
+$result .= "</tbody></table>";
+return $result;
+}
+
 
 }
 
